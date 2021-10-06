cbd516babdf6682bc1933340b964fc43f87b158b
GabrielBarrosAffonso <gabrielbarrosaffonso@gmail.com>
2021-10-06 13:56:56 -0300

    Git User test

--- src/components/MainForm/MainForm.jsx
+++ src/components/MainForm/MainForm.jsx
@@ -5,6 +5,7 @@ function MainForm(){
     return(
         <div>
             <form className="">
+                <p>Oi</p>
                 <TextField id="name" label="Name" variant="outlined" fullWidth={true} margin="dense"/>
 
                 <TextField id="surname" label="Surname" variant="outlined" fullWidth={true} margin="dense"/>
