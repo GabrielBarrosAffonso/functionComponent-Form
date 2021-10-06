700d89310fa26a69d1667c8cb1724109f6cc53b8
GabrielBarrosAffonso <gabrielbarrosaffonso@gmail.com>
2021-10-06 16:25:50 -0300

    States for the full form added, props example added

--- src/App.js
+++ src/App.js
@@ -6,9 +6,13 @@ function App() {
   return (
     <Container component="article" maxWidth="sm">
           <h1>FORMULÁRIO DE CADASTRO</h1>
-          <MainForm />
+          <MainForm onSend={onSendForm}/>
     </Container>
   );
+}
+
+function onSendForm(data){
+  console.log(data)
 }
 
 export default App;
--- src/components/MainForm/MainForm.jsx
+++ src/components/MainForm/MainForm.jsx
@@ -1,24 +1,26 @@
 import React, {useState} from "react";
 import { TextField , Button , Switch , FormControlLabel } from "@material-ui/core";
 
-function MainForm(){
+function MainForm({onSend}){
     const [name, setName] = useState("")
     const [surname, setSurname] = useState("")
+    const [cpf, setCpf] = useState("")
+    const [offers, setOffers] = useState(true)
+    const [news, setNews] = useState(true)
 
     return(
         <div>
             <form onSubmit={(e) => {
                 e.preventDefault();
-                console.log(name)
-                console.log(surname)
+                onSend({name, surname, cpf, news, offers})
             }}>
 
                 <TextField 
                 value={name}
                 onChange={(e) => {
                     let tempName = e.target.value;
-                    if(tempName.length >= 5){
-                        tempName = tempName.substr(0, 5);
+                    if(tempName.length >= 15){
+                        tempName = tempName.substr(0, 15);
                     }
                     setName(tempName)
                 }} 
@@ -40,20 +42,28 @@ function MainForm(){
                 margin="dense"/>
 
                 <TextField 
+                value={cpf}
+                onChange={(e) => {
+                    setCpf(e.target.value)
+                }}
                 id="cpf" 
                 label="CPF" 
                 variant="outlined"
                 fullWidth={true} 
                 margin="dense"/>
 
-                <FormControlLabel 
+                <FormControlLabel
                 label="Offers" 
-                control={<Switch name="offers" defaultChecked />}/>
+                control={<Switch onChange={(e) => {
+                            setOffers(e.target.checked)
+                        }} checked={offers} name="offers"/>}/>
                 {/* <Switch name="offers" defaultChecked /> */}
 
                 <FormControlLabel 
                 label="News" 
-                control={<Switch name="news" defaultChecked />}/>
+                control={<Switch onChange={(e) => {
+                            setNews(e.target.checked)
+                        }} checked={news} name="news"/>}/>
                 {/* <Switch name="news" defaultChecked /> */}
 
                 <Button variant="contained" color="primary" type="submit">
